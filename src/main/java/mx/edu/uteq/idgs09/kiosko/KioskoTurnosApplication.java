ab08947f226d818b1524580c18b31b1ce56e4c7e
BrySan20 <bryansanchez1680@gmail.com>
2025-06-03 13:09:38 -0600

    Proyecto backend limpio Spring boot

---
+++ src/main/java/mx/edu/uteq/idgs09/kiosko/KioskoTurnosApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs09.kiosko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskoTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskoTurnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/uteq/idgs09/kiosko/KioskoTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs09.kiosko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskoTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
